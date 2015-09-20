5206a2977095fbf1641530ef52d02e66aa9801d9
unknown <marceloquintana2@gmail.com>
2015-09-20 15:52:36 -0400

    archivo cambiado

--- mu.cpp
+++ mu.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	cout<<"MUUUU"<<endl;
+	int a ;
+	
+  	freopen ("input.txt","r",stdin);
+  	freopen ("output.txt","w",stdout);
+  	cin >> a;
+	cout<<a<<endl;
 	return 0;
 }
